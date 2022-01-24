2e229bc9978a33340c6d9ac8705f691192b2ef6f
Charles McLean <mcdiesen@gmail.com>
2022-01-24 15:45:54 -0600

    Testing and Benchmarking

---
+++ testing/test/01/main.go
@@ -0,0 +1,19 @@
+//Package test main
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("La suma de 2+4 es: ", miSuma(2, 4))
+	fmt.Println("La suma de 6+4 es: ", miSuma(6, 4))
+
+	fmt.Println("La suma de 10+4 es: ", miSuma(10, 4))
+}
+
+func miSuma(xi ...int) int {
+	var sum int
+	for _, v := range xi {
+		sum += v
+	}
+	return sum
+}
